c053a6aa609d4a43e2aa5bd29b72ae7fbbf292dc
unknown <axelgrullon@outlook.com>
2021-05-13 00:32:08 -0400

    Anagram-Finder-1.0.1

--- main.test.js
+++ main.test.js
@@ -1,7 +1,11 @@
 var pa1 = "kaka"
-var pa2 = "akak"
+var pa2 = "akat"
 
 const pruebaAnagrama = require("./main")
 test("Retorna verdadero cuando el anagrama existe.", () => {
 	expect(pruebaAnagrama(pa1, pa2)).toBe(true);
+}),
+
+test("Retorna falso cuando el anagrama no exista.", () => {
+	expect(pruebaAnagrama(pa1, pa2)).toBe(false);
 })
